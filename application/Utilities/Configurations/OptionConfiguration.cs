16247e747b5bd850019072eb784443d4db80c8a1
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2026-01-18 17:14:58 +0700

    update options configuration and make it required

--- application/Utilities/Configurations/OptionConfiguration.cs
+++ application/Utilities/Configurations/OptionConfiguration.cs
@@ -1,16 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 using FoodSphere.Configurations.Options;
+using Microsoft.Extensions.Options;
 
+// # which is better?
+// - https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options
+// - https://learn.microsoft.com/en-us/dotnet/core/extensions/options-validation-generator
 public static class ServiceOptionExtensions
 {
     public static void AddFoodSphereOptions(this IServiceCollection services, ConfigurationManager config)
     {
         services.AddOptions<ConnectionStringsOption>()
             .Bind(config.GetSection(ConnectionStringsOption.SectionName))
-            .ValidateDataAnnotations()
-            .ValidateOnStart();
-
-        services.AddOptions<JwtOption>()
-            .Bind(config.GetSection(JwtOption.SectionName))
             .ValidateDataAnnotations()
             .ValidateOnStart();
 
@@ -23,6 +23,11 @@ public static class ServiceOptionExtensions
             .Bind(config.GetSection(GoogleOption.SectionName))
             .ValidateDataAnnotations()
             .ValidateOnStart();
+
+        services.AddOptions<DomainOption>()
+            .Bind(config.GetSection(DomainOption.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
     }
 }
 
@@ -32,7 +37,9 @@ namespace FoodSphere.Configurations.Options
     {
         public const string SectionName = "Google";
 
+        [Required]
         public required string client_id { get; init; }
+        [Required]
         public required string client_secret { get; init; }
     }
 
@@ -40,21 +47,17 @@ namespace FoodSphere.Configurations.Options
     {
         public const string SectionName = "ConnectionStrings";
 
-        public required string foodsphere { get; init; }
-    }
-
-    public class JwtOption
-    {
-        public const string SectionName = "Jwt";
-
-        public required string signing_key { get; init; }
+        [Required]
+        public required string @default { get; init; }
     }
 
     public class MagicLinkOption
     {
         public const string SectionName = "MagicLink";
 
+        [Required]
         public required string signing_key { get; init; }
+        [Required]
         public required string encryption_key { get; init; }
     }
 
@@ -62,6 +65,39 @@ namespace FoodSphere.Configurations.Options
     {
         public const string SectionName = "KeyVault";
 
+        [Required]
         public required string uri { get; init; }
+    }
+
+    public class DomainOption
+    {
+        public const string SectionName = "Domain";
+
+        [ValidateObjectMembers]
+        public required NestdeDomainOption api { get; init; }
+
+        [ValidateObjectMembers]
+        public required NestdeDomainOption admin { get; init; }
+
+        [ValidateObjectMembers]
+        public required NestdeDomainOption pos { get; init; }
+
+        [ValidateObjectMembers]
+        public required NestdeDomainOption master { get; init; }
+
+        [ValidateObjectMembers]
+        public required NestdeDomainOption consumer { get; init; }
+
+        [ValidateObjectMembers]
+        public required NestdeDomainOption ordering { get; init; }
+    }
+
+    public class NestdeDomainOption
+    {
+        [Required]
+        public required string url { get; init; }
+
+        [Required]
+        public required string signing_key { get; init; }
     }
 }
