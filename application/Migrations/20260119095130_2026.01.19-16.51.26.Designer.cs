3b073bc789fcf8e9219a0332a4773921e06258fc
Icarus <63220735+shoguncoffee@users.noreply.github.com>
2026-01-19 21:45:59 +0700

    update migration

--- application/Migrations/20260119095130_2026.01.19-16.51.26.Designer.cs
+++ application/Migrations/20260119095130_2026.01.19-16.51.26.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FoodSphere;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace foodsphereapi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260119095130_2026.01.19-16.51.26")]
+    partial class _20260119165126
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
